15f7d0d21a6210608fcc24f1905dd45965c3d471
LeidyRosmery <rosmery.maldonado89@gmail.com>
2018-01-26 14:25:42 -0500

    add csss back to school

---
+++ js/carrousel.js
@@ -0,0 +1,149 @@
+//
+// $(".slider-drag1").slick({
+//   dots: true,
+//   adaptiveHeight: false,
+//   centerPadding: "10px",
+//   mobileFirst: true,
+//   infinite: true,
+//    // slidesToShow: 1,
+//     speed: 100,
+//   slidesToScroll: 1,
+//   arrows: false,
+//
+//   centerMode: false,
+//
+//   responsive: [{
+//     breakpoint: 992,
+//     settings: {
+//       slidesToShow: 1,
+//       // autoplay:true,
+//        autoplaySpeed: 5000,
+//       arrows: false
+//
+//     }
+//   }]
+// });
+//
+// $(".slider-drag2").slick({
+//   dots: true,
+//   adaptiveHeight: false,
+//   centerPadding: "10px",
+//   mobileFirst: true,
+//   infinite: true,
+//    // slidesToShow: 2,
+//     speed: 100,
+//   slidesToScroll: 1,
+//   arrows: false,
+//  variableWidth: true,
+//   centerMode: false,
+//
+//   responsive: [{
+//     breakpoint: 1024,
+//     settings: {
+//       // slidesToShow: 1,
+//       swipe:false,
+//       arrows: false,
+//       infinite:false
+//
+//     }
+//   }]
+// });
+
+  var configSlick = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    vertical: false,
+    infinite: false,
+    prevArrow: $('.bPrev'),
+    nextArrow: $('.bNext'),
+    responsive: [{
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  };
+
+
+
+  var events = {
+    init: function() {
+      // this.changeHomeSlider();
+      this.initHomeSlider();
+      this.initFooterSlider();
+
+    },
+
+    initHomeSlider: function() {
+      if ($('.slider-drag1').length) {
+        $('.slider-drag1').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: true,
+          vertical: false,
+          infinite: true,
+          responsive: [{
+              breakpoint: 700,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                arrows:false,
+                dots: true
+              }
+            }
+          ]
+          // prevArrow: $('.sPrev'),
+          // nextArrow: $('.sNext')
+        });
+      }
+    },
+    initFooterSlider: function() {
+      if ($('.slider-drag2').length) {
+        $('.slider-drag2').slick({
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          arrows: true,
+          swipe: true,
+          // autoplay: true,
+          autoplaySpeed: 500,
+          responsive: [{
+              breakpoint: 700,
+              settings: {
+                slidesToShow: 2,
+                  slidesToScroll: 1,
+                dots: true,
+                arrows: false,
+                swipe: true
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+                  slidesToScroll: 1,
+                variableWidth: true,
+                arrows: false,
+                dots: true,
+                swipe: true
+              }
+            }
+          ]
+        });
+      }
+    }
+  };
+  events.init();
---
+++ js/video-tip.js
@@ -0,0 +1,41 @@
+var contenidoCurrentVideo = [{
+  video: "https://www.youtube.com/watch?v=Ginx7WKq5GE&list=LLbFy2AGQPMDoDoRqgtPHTdQ&index=21",
+  img: "img/logo-escritorio.png",
+  texto: "Aprovecha un rincón de tu dormitorio y conviértelo en tu zona de estudio. Aprende a integrar un escritorio en tu habitación con los siguientes consejos."
+}, {
+  video: "https://www.youtube.com/watch?v=fKopy74weus&index=24&list=LLbFy2AGQPMDoDoRqgtPHTdQ",
+  img: "img/icono-escritorio.png",
+  texto: "Una área de estudio ordenada incrementa la atención y rendimiento académico. Clasifica tus objetos de uso diario y evita el desorden con los siguientes tips."
+}, {
+  video: "https://www.youtube.com/watch?v=PMivT7MJ41M&list=LLbFy2AGQPMDoDoRqgtPHTdQ&index=28",
+  img: "img/logo-banner.png",
+  texto: "Una buena alimentación estimula la concentración y renueva la energía durante el día. Prepara una lonchera saludable siguiendo estos consejos."
+}];
+$(document).ready(function() {
+  var widthWindows = $(window).width();
+  if (widthWindows > 992) {
+    $('.link-video-tip').on('click', function(e) {
+      // $('.video-tip-current').fadeOut('slow');
+
+      // $(".video-tip-current").fadeTo("slow",0);
+
+      e.preventDefault();
+      $('.img-current ').attr('src', contenidoCurrentVideo[$(this).data('video')].img);
+      $('.text-current').html(contenidoCurrentVideo[$(this).data('video')].texto);
+      var currentVideo = $(this).find('[class*=video-tip-]').css('background-image');
+      $(".video-tip-current").css("background-image", currentVideo);
+        // $('.video-tip-current').fadeIn('fast');
+        // $(".video-tip-current").fadeTo("slow",1);
+
+    })
+  }
+});
+$(window).resize(function() {
+
+  if (widthWindows > 992) {
+    $('.link-video-tip').on('click', function(e) {
+      e.preventDefault();
+
+    })
+  }
+});
